2d88707b1cdf28b125667669e3d4888f8f938204
Jeremias <jeremontoya155@gmail.com>
2024-01-22 10:10:20 -0300

    Cambios Subidos

--- server.js
+++ server.js
@@ -12,18 +12,29 @@ app.get('/', (req, res) => {
 
 app.get('/buscar-modelos', async (req, res) => {
   try {
-    const brandId = req.query.brandId || 6; // ID de la marca AUDI (valor predeterminado: 6)
-    const groupId = req.query.groupId || 10; // ID del grupo (valor predeterminado: 10)
+    const brandId = parseInt(req.query.brandId) || 6; // ID de la marca (valor predeterminado: 6)
+    const startGroupId = parseInt(req.query.startGroupId) || 6; // ID de inicio de grupo (valor predeterminado: 6)
+    const endGroupId = parseInt(req.query.endGroupId) || 15; // ID de fin de grupo (valor predeterminado: 15)
     const apiUrl = 'https://api.infoauto.com.ar/cars/pub/';
     const accessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTcwNTkyODExMywianRpIjoiNDhkMGYwMzUtYWI0OS00YzFmLTgxZGQtNjVlMWRjZWRlMGY0IiwidHlwZSI6ImFjY2VzcyIsImlkZW50aXR5IjozOTEsIm5iZiI6MTcwNTkyODExMywiZXhwIjoxNzA1OTMxNzEzLCJyb2xlcyI6W3siaWQiOjExLCJuYW1lIjoiMGttIn0seyJpZCI6MTksIm5hbWUiOiJEZXNhcnJvbGxvIn0seyJpZCI6MTAsIm5hbWUiOiJFeHRyYXMifSx7ImlkIjo5LCJuYW1lIjoiTW9kZWxvcyJ9LHsiaWQiOjEyLCJuYW1lIjoiVXNhZG9zIn1dfQ.NuT5aOh_v0SsDC41yMYS9XTd7c_8D2sM4y4B4fq5ULw';
 
-    const response = await axios.get(`${apiUrl}/brands/${brandId}/groups/${groupId}/models/`, {
-      headers: {
-        'Authorization': `Bearer ${accessToken}`
-      }
-    });
+    let modelos = [];
 
-    const modelos = response.data;
+    async function hacerSolicitudes(groupId) {
+      const response = await axios.get(`${apiUrl}/brands/${brandId}/groups/${groupId}/models/`, {
+        headers: {
+          'Authorization': `Bearer ${accessToken}`
+        }
+      });
+
+      modelos = modelos.concat(response.data);
+
+      if (groupId < endGroupId) {
+        await hacerSolicitudes(groupId + 1);
+      }
+    }
+
+    await hacerSolicitudes(startGroupId);
 
     res.json({ modelos });
   } catch (error) {
