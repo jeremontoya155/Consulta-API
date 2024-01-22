1d5c92c6a9e57dc8820803b7e92abaec3580ee48
Jeremias <jeremontoya155@gmail.com>
2024-01-22 11:02:53 -0300

    Cambios para conectar los codias

--- server.js
+++ server.js
@@ -12,11 +12,11 @@ app.get('/', (req, res) => {
 
 app.get('/buscar-modelos', async (req, res) => {
   try {
-    const brandId = parseInt(req.query.brandId) || 6; // ID de la marca (valor predeterminado: 6)
-    const startGroupId = parseInt(req.query.startGroupId) || 6; // ID de inicio de grupo (valor predeterminado: 6)
-    const endGroupId = parseInt(req.query.endGroupId) || 15; // ID de fin de grupo (valor predeterminado: 15)
+    const brandId = parseInt(req.query.brandId) || 18; // ID de la marca (valor predeterminado: 6)
+    const startGroupId = parseInt(req.query.startGroupId) || 1; // ID de inicio de grupo (valor predeterminado: 6)
+    const endGroupId = parseInt(req.query.endGroupId) || 25; // ID de fin de grupo (valor predeterminado: 15)
     const apiUrl = 'https://api.infoauto.com.ar/cars/pub/';
-    const accessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTcwNTkyODExMywianRpIjoiNDhkMGYwMzUtYWI0OS00YzFmLTgxZGQtNjVlMWRjZWRlMGY0IiwidHlwZSI6ImFjY2VzcyIsImlkZW50aXR5IjozOTEsIm5iZiI6MTcwNTkyODExMywiZXhwIjoxNzA1OTMxNzEzLCJyb2xlcyI6W3siaWQiOjExLCJuYW1lIjoiMGttIn0seyJpZCI6MTksIm5hbWUiOiJEZXNhcnJvbGxvIn0seyJpZCI6MTAsIm5hbWUiOiJFeHRyYXMifSx7ImlkIjo5LCJuYW1lIjoiTW9kZWxvcyJ9LHsiaWQiOjEyLCJuYW1lIjoiVXNhZG9zIn1dfQ.NuT5aOh_v0SsDC41yMYS9XTd7c_8D2sM4y4B4fq5ULw';
+    const accessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTcwNTkzMTgzMywianRpIjoiYjNkMDllYjUtNTc1Ni00ZmFiLTk2ZjUtMDdkZTlhZWRlYTFiIiwidHlwZSI6ImFjY2VzcyIsImlkZW50aXR5IjozOTEsIm5iZiI6MTcwNTkzMTgzMywiZXhwIjoxNzA1OTM1NDMzLCJyb2xlcyI6W3siaWQiOjExLCJuYW1lIjoiMGttIn0seyJpZCI6MTksIm5hbWUiOiJEZXNhcnJvbGxvIn0seyJpZCI6MTAsIm5hbWUiOiJFeHRyYXMifSx7ImlkIjo5LCJuYW1lIjoiTW9kZWxvcyJ9LHsiaWQiOjEyLCJuYW1lIjoiVXNhZG9zIn1dfQ.9APA6mvsfqNphLplqN0Qar3qyTOxiWfOJpnlwUfA0ys';
 
     let modelos = [];
 
